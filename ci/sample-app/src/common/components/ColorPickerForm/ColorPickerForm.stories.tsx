a7c90aba7b9c7554a0e1a1279506477a3d3be1c3
Jonathan Cheng <7626923-jchengdev@users.noreply.gitlab.com>
2022-03-29 21:39:56 -0300

    feature: ColorPickerForm component

---
+++ ci/sample-app/src/common/components/ColorPickerForm/ColorPickerForm.stories.tsx
@@ -0,0 +1,27 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import ColorPickerForm from '.';
+
+import seeds from '@/common/_seeds';
+
+export default {
+  title: 'Components/ColorPickerForm',
+  component: ColorPickerForm,
+  argTypes: {
+    addNewColor: { action: 'ADD NEW COLOR' },
+  },
+} as ComponentMeta<typeof ColorPickerForm>;
+
+const Template: ComponentStory<typeof ColorPickerForm> = args => (
+  <div
+    style={{ top: '10px', marginLeft: '10px', height: '20vh', width: '30vw' }}
+  >
+    <ColorPickerForm {...args} />
+  </div>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  paletteIsFull: false,
+  colors: seeds[0]?.colors,
+};
