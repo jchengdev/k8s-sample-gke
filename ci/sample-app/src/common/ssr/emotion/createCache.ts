0763ea3cc2466eea3e954630bc70362bde67b4fb
Jonathan Cheng <30513739+jchengdev@users.noreply.github.com>
2022-07-15 18:20:03 -0300

    refactor(ci-cd-app): emotion cache cleanup

--- ci/sample-app/src/common/ssr/emotion/createCache.ts
+++ ci/sample-app/src/common/ssr/emotion/createCache.ts
@@ -1,5 +1,7 @@
 import createCache from '@emotion/cache';
 
+import { EMOTION_CACHE_KEY, EMOTION_INSERTION_POINT } from './_constants';
+
 const isBrowser = typeof document !== 'undefined';
 
 // On the client side, Create a meta tag at the top of the <head> and set it as insertionPoint.
@@ -10,16 +12,16 @@ function createEmotionCache() {
 
   if (isBrowser) {
     const emotionInsertionPoint = document.querySelector<HTMLMetaElement>(
-      'meta[name="emotion-insertion-point"]'
+      `meta[name="${EMOTION_INSERTION_POINT}"]`
     );
     insertionPoint = emotionInsertionPoint ?? undefined;
 
     console.log(
-      `BROWSER side, insertionPoint: ${JSON.stringify(emotionInsertionPoint)}`
+      `BROWSER side, insertionPoint: ${emotionInsertionPoint?.outerHTML}`
     );
   }
 
-  return createCache({ key: 'emotion-cache', insertionPoint });
+  return createCache({ key: EMOTION_CACHE_KEY, insertionPoint });
 }
 
 export { createEmotionCache };
---
+++ ci/sample-app/src/common/ssr/emotion/headStyles.tsx
@@ -0,0 +1,35 @@
+import type { EmotionCriticalToChunks } from '@emotion/server/create-instance';
+import { EMOTION_INSERTION_POINT } from './_constants';
+
+const generateEmotionStyles = (
+  html: string,
+  stylesExtractor: (html: string) => EmotionCriticalToChunks
+): JSX.Element[] => {
+  // This is important. It prevents Emotion to render invalid HTML.
+  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
+  // console.log(`INITIALPROPSHTML: ${JSON.stringify(html)}`);
+  const emotionStyles = stylesExtractor(html);
+  // console.log(`EMOTIONSTYLES: ${JSON.stringify(emotionStyles)}`);
+  const emotionStyleTags = emotionStyles.styles.map(style => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return emotionStyleTags;
+};
+
+const injectEmotionStyles = (styles: React.ReactNode[]): JSX.Element => {
+  console.log(`INJECTED STYLES: ${JSON.stringify(styles)}`);
+  return (
+    <>
+      {styles}
+      <meta name={EMOTION_INSERTION_POINT + '-end'} content="" />
+    </>
+  );
+};
+
+export { generateEmotionStyles, injectEmotionStyles };
