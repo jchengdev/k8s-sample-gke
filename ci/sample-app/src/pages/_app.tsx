aeb9f0cd1374a008191d04a2e128e3575f44efef
Jonathan Cheng <30513739+jchengdev@users.noreply.github.com>
2022-07-15 13:51:42 -0300

    fix: emotion cache CSS injection

--- ci/sample-app/src/pages/_app.tsx
+++ ci/sample-app/src/pages/_app.tsx
@@ -1,10 +1,28 @@
 import type { AppProps } from 'next/app';
 
+import type { EmotionCache } from '@emotion/cache';
+import { createEmotionCache } from '@/common/ssr/emotion/createCache';
+
 import { GlobalContainer } from '@/common/containers/GlobalContainer';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+// Client-side cache, shared for the whole session of the user in the browser.
+// ! IMPORTANT: make sure this only runs once, as multiple runs may produce different style hashes
+const clientSideEmotionCache: EmotionCache = (() => {
+  console.log('clientSideEmotionCache() called');
+  return createEmotionCache();
+})();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const MyApp = ({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: MyAppProps) => {
   return (
-    <GlobalContainer>
+    <GlobalContainer emotionCache={emotionCache}>
       <Component {...pageProps} />
     </GlobalContainer>
   );
